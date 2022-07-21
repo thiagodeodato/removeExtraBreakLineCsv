c9fcba207af005b5e2a7ac6bfffc29d9bba90949
Thiago Deodato <thiago-deodato@hotmail.com>
2022-07-21 08:52:59 -0300

    removing usage of header.csv for counting rows

--- breakLine.js
+++ breakLine.js
@@ -1,31 +1,25 @@
 const fs = require('fs');
 const readline = require('readline');
-var pathHeaderFile = 'arquivos/Header.csv';
-var pathReadFile = 'arquivos/biAntigo/2018/Billing_20210623231641578.csv';
-var pathWriteFile = 'arquivos/biCorrigido/2018/BillingCorrigido.csv';
-
-/* function countSeparators (pathHeaderFile) {
-  const file = fs.readFileSync(pathHeaderFile, 'utf8')
-    
-    console.log('qnt de ; da linha ->',(file.match(/;/g) || []).length)
-    return (file.match(/;/g) || []).length;
-} */
+//var pathHeaderFile = 'header.csv';
+var pathReadFile = 'testIn.csv';
+var pathWriteFile = 'testOut.csv';
 
 function removeBreakLine (pathReadFile, pathWriteFile) {
   const rl = readline.createInterface({
     input: fs.createReadStream(pathReadFile, {encoding: 'latin1'}),
     crlfDelay: Infinity
   });
-  
+
   var semiColonQuantity = 0;
   var qntLinhaAtual = 0;
   var linhaAtual = 1;
   var conteudoLinha;
   var timeBeforeExecute = new Date(Date.now());
-  console.time("breakLine");
+  //console.time("breakLine");
   rl.on('line', (line) => {
     if (linhaAtual <= 1) {
-      console.log('qnt de ; da linha ->',(line.match(/;/g) || []).length);
+      // Counting ';' in read file header
+      //console.log('qnt de ; da linha ->',(line.match(/;/g) || []).length);
       semiColonQuantity = (line.match(/;/g) || []).length;
       linhaAtual++;
     } else{
@@ -46,10 +40,8 @@ function removeBreakLine (pathReadFile, pathWriteFile) {
       linhaAtual++;
     }
   });
-  console.timeEnd("breakLine");
+  //console.timeEnd("breakLine");
 }
 
-//semiColonQuantity = getFirstLine(pathHeaderFile);
+
 removeBreakLine (pathReadFile, pathWriteFile);
-
-
